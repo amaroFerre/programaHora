18c0ce0d90f9fe8e3d790881ece3c3dae0be9ca3
Amarico <Amarico@equipo-zalman>
2018-09-06 18:16:41 +0200

    Realiza un programa que pida una hora por teclado y que muestre luego buenos días, buenas tardes o buenas noches según la hora. Se utilizarán los tramos de 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta  las horas, los minutos no se deben introducir por teclado.

---
+++ src/programahora/ProgramaHora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programahora;
+
+/**
+ * Realiza un programa que pida una hora por teclado y que muestre luego buenos
+ * días, buenas tardes o buenas noches según la hora. Se utilizarán los tramos
+ * de 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta
+ * las horas, los minutos no se deben introducir por teclado.
+ *
+ * @author Amarico
+ */
+public class ProgramaHora {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
